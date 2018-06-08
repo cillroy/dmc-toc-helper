cd88b9394fedd4812c6e299b31494344128358ca
cillroy <curt@cullens.com>
2018-06-08 14:26:11 -0700

    show debug with querystring ?debug

--- includes/toc-helper.js
+++ includes/toc-helper.js
@@ -71,7 +71,35 @@ function manageNodeTitles() {
     }
 }
 
+function checkParemeterExists(parameter)
+{
+   //Get Query String from url
+   fullQString = window.location.search.substring(1);
+   
+   paramCount = 0;
+   queryStringComplete = "?";
+
+   if(fullQString.length > 0)
+   {
+       //Split Query String into separate parameters
+       paramArray = fullQString.split("&");
+       
+       //Loop through params, check if parameter exists.  
+       for (i=0;i<paramArray.length;i++)
+       {
+         currentParameter = paramArray[i].split("=");
+         if(currentParameter[0] == parameter) //Parameter already exists in current url
+         {
+            return true;
+         }
+       }
+   }
+   
+   return false;
+}
+
 $(function () {
+    if (checkParemeterExists("debug")){ $("#debug").show()}
 
     $("#copy").click(function () {
         //var copyTextarea = document.querySelector('codeText');
